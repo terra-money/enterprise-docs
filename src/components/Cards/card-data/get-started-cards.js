c4f85f90d5d0ca62f1791bee0679a3f22611b070
evanorti <87997759+evanorti@users.noreply.github.com>
2022-11-08 15:59:14 -0800

    Create get-started-cards.js

---
+++ src/components/Cards/card-data/get-started-cards.js
@@ -0,0 +1,46 @@
+const getStartedCards = [
+  {
+    href: 'guides/multisig.html',
+    lightSvg: '/img/icons/buildanapp01.svg',
+    darkSvg: '/img/icons/dark/buildanapp01.svg',
+    header: 'Multisig guide',
+    summary: 'Create a multisig wallet DAO.'
+  },
+  {
+    href: 'guides/token.html',
+    lightSvg: '/img/icons/toolguide.svg',
+    darkSvg: '/img/icons/dark/toolguide.svg',
+    header: 'Token DAO guide',
+    summary: 'Create a Community Token DAO.'
+  },
+  {
+    href: 'guides/nft.html',
+    lightSvg: '/img/icons/terrain.svg',
+    darkSvg: '/img/icons/dark/terrain.svg',
+    header: 'NFT DAO guide',
+    summary: 'Create an NFT Community DAO.'
+  },
+  {
+    href: 'guides/stake.html',
+    lightSvg: '/img/icons/terrain.svg',
+    darkSvg: '/img/icons/dark/terrain.svg',
+    header: 'Staking guide',
+    summary: 'Stake your assets to participate in governance.'
+  },
+  {
+    href: 'guides/governance.html',
+    lightSvg: '/img/icons/terrain.svg',
+    darkSvg: '/img/icons/dark/terrain.svg',
+    header: 'Governance guide',
+    summary: 'Learn how to create and vote on proposals'
+  },
+  {
+    href: 'concepts/about.html',
+    lightSvg: '/img/icons/terrain.svg',
+    darkSvg: '/img/icons/dark/terrain.svg',
+    header: 'What is a DAO?',
+    summary: 'Learn about the different types of DAOs.'
+  }
+]
+
+export default getStartedCards;
