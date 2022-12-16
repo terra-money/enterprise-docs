00e9087597e360d714300102a2741dcbf69f46cd
evanorti <87997759+evanorti@users.noreply.github.com>
2022-12-16 13:11:52 -0800

    Update get-started-cards.ts

--- src/components/Cards/card-data/get-started-cards.ts
+++ src/components/Cards/card-data/get-started-cards.ts
@@ -2,13 +2,6 @@ import { CardProps } from '../Card';
 
 const getStartedCards: CardProps[] = [
   {
-    href: '/concepts/about',
-    lightSVG: '/img/cards/question.svg',
-    darkSVG: '/img/cards/question.svg',
-    header: 'What is a DAO?',
-    summary: 'Learn about the different types of DAOs.'
-  },
-  {
     href: '/guides/multisig',
     lightSVG: '/img/cards/wallet2.svg',
     darkSVG: '/img/cards/wallet2.svg',
@@ -36,13 +29,6 @@ const getStartedCards: CardProps[] = [
     header: 'Staking guide',
     summary: 'Stake your assets to participate in governance.'
   },
-  {
-    href: '/guides/governance',
-    lightSVG: '/img/cards/vote.svg',
-    darkSVG: '/img/cards/vote.svg',
-    header: 'Governance guide',
-    summary: 'Learn how to create and vote on proposals'
-  }
 ]
 
 export default getStartedCards;
