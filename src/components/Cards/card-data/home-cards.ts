763a360c43c85146970e9c8094ed01affedcca81
evanorti <87997759+evanorti@users.noreply.github.com>
2022-12-16 13:11:53 -0800

    Create home-cards.ts

---
+++ src/components/Cards/card-data/home-cards.ts
@@ -0,0 +1,48 @@
+import { CardProps } from '../Card';
+
+const homeCards: CardProps[] = [
+  {
+    href: '/concepts/about',
+    lightSVG: '/img/cards/question.svg',
+    darkSVG: '/img/cards/question.svg',
+    header: 'What is a DAO?',
+    summary: 'Learn about the different types of DAOs.'
+  },
+  {
+    href: '/guides/multisig',
+    lightSVG: '/img/cards/wallet2.svg',
+    darkSVG: '/img/cards/wallet2.svg',
+    header: 'Multisig guide',
+    summary: 'Create a multisig wallet DAO.'
+  },
+  {
+    href: '/guides/token',
+    lightSVG: '/img/cards/bank.svg',
+    darkSVG: '/img/cards/bank.svg',
+    header: 'Token DAO guide',
+    summary: 'Create a Community Token DAO.'
+  },
+  {
+    href: '/guides/nft',
+    lightSVG: '/img/cards/nft.svg',
+    darkSVG: '/img/cards/nft.svg',
+    header: 'NFT DAO guide',
+    summary: 'Create an NFT Community DAO.'
+  },
+  {
+    href: '/guides/stake',
+    lightSVG: '/img/cards/stake.svg',
+    darkSVG: '/img/cards/stake.svg',
+    header: 'Staking guide',
+    summary: 'Stake your assets to participate in governance.'
+  },
+  {
+    href: '/guides/governance',
+    lightSVG: '/img/cards/vote.svg',
+    darkSVG: '/img/cards/vote.svg',
+    header: 'Governance guide',
+    summary: 'Learn how to create and vote on proposals'
+  }
+]
+
+export default homeCards;
